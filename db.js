8ee823057834cf9ca07f72f4dd7be65094ebf0e9
Ankit Kumar Verma <av95766@gmail.com>
2024-04-18 11:00:28 +0530

    removed token

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
-const { Sequelize } = require("sequelize");
+const dotenv = require('dotenv');
+dotenv.config();
 
-const sequelize = new Sequelize("ankitverma", "chpl", "Chpl@321.", {
-  host: "localhost",
-  dialect: "mysql",
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
 });
 
-sequelize
-  .authenticate()
+// Test the connection
+sequelize.authenticate()
   .then(() => {
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
   })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
   });
 
 module.exports = { sequelize };
